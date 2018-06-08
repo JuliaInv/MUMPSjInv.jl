bdd8480f687e9036191e980b2b76c3b02c76aa3b
Patrick Belliveau <patrick.t.belliveau@gmail.com>
2018-06-08 11:39:33 -0700

    Fix sparse rhs support, add tests for it and start using test sets.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
+using Base.Test
+
 println("test MUMPS")
-include("testDivGrad.jl")
-include("testTwoSystem.jl")
-include("testTwoSystemParallel.jl")
-include("testSparseRHS.jl")
-println("Done!")
+@testset "MUMPS" begin
+    @testset "DivGrad" begin include("testDivGrad.jl") end
+    @testset "Two systems" begin include("testTwoSystem.jl") end
+    @testset "Two systems parallel" begin include("testTwoSystemParallel.jl") end
+    @testset "Sparse RHS" begin include("testSparseRHS.jl") end
+    println("Done!")
+end
